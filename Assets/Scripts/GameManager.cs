15d6485b6db61cbb6ddb9a62268a40fb60f11877
Eflsane <kireevroma01@yandex.ru>
2021-10-29 17:37:16 +0400

    fixed gitignore, loaded ready files from local project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public static bool isConstructMode = false;
+    public static bool isColoringGrid = false;
+    public static bool canPlace = false;
+
+    public ConstructElemController[] constructElems = new ConstructElemController[1];
+    public ConstructElemUIController[] __constructElemsUI;
+
+    public static ConstructElemController currentElemSelected;
+    public static GridElemController currentGridElemSelected;
+    public static GridElemController prevGridElemSelected;
+
+    public GameObject mainPanel;
+    public GameObject constructPanel;
+    public GameObject placingItemPanel;
+    public GameObject placeButton;
+
+    public TMPro.TextMeshProUGUI scoreCounterText;
+    public static float scoreCounter;
+    public float startScore;
+    public static float income;
+    public float basicIncome;
+    public static float scoreMultypler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance == this)
+            Destroy(gameObject);
+
+        //__constructElemsUI = new ConstructElemUIController[constructElems.Length];
+
+        scoreCounter = startScore;
+        income = basicIncome;
+        scoreMultypler = 1.0f;
+        scoreCounterText.text = "$ " + scoreCounter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreCounterText.text = "$ " + string.Format("{0:0.0}", scoreCounter);
+        scoreCounter += income * scoreMultypler * Time.deltaTime;
+
+        for(int i = 0; i < __constructElemsUI.Length; i++)
+        {
+            if(scoreCounter < constructElems[i].price)
+            {
+                __constructElemsUI[i].GetComponent<Button>().interactable = false;
+            }
+            else
+            {
+                __constructElemsUI[i].GetComponent<Button>().interactable = true;
+            }
+        }
+
+        if(currentElemSelected != null && currentGridElemSelected != null)
+        {
+            if (!currentElemSelected.Equals(currentGridElemSelected.installedObject))
+            {
+                placeButton.GetComponent<Button>().interactable = false;
+                Debug.Log("false");
+            }
+            else
+            {
+                placeButton.GetComponent<Button>().interactable = true;
+                Debug.Log("true");
+            }
+        }
+        
+    }
+
+    public void SwitchContructionMode()
+    {
+        isColoringGrid = true;
+
+        if (isConstructMode)
+        {
+            isConstructMode = false;
+            mainPanel.SetActive(true);
+            constructPanel.SetActive(false);
+        }
+
+        else
+        {
+            isConstructMode = true;
+            mainPanel.SetActive(false);
+            constructPanel.SetActive(true);
+        }
+    }
+
+    public void SwitchPlacingMode()
+    {
+        if (isConstructMode && placingItemPanel.activeInHierarchy)
+        {
+            constructPanel.SetActive(true);
+            placingItemPanel.SetActive(false);
+
+        }
+
+        else
+        {
+            constructPanel.SetActive(false);
+            placingItemPanel.SetActive(true);
+        }
+    }
+
+    public void CancelPlacing()
+    {
+        if (currentElemSelected.Equals(currentGridElemSelected.installedObject))
+            currentGridElemSelected.ChangeColor(currentGridElemSelected.buildingOKColor);
+        Destroy(currentElemSelected.gameObject);
+        currentGridElemSelected = null;
+        SwitchPlacingMode();
+    }
+
+    public void FinishPlacingItemOnGrid()
+    {
+        income += currentElemSelected.incomeAscending;
+        scoreCounter -= currentElemSelected.price;
+        currentElemSelected = null;
+        currentGridElemSelected = null;
+        SwitchPlacingMode();
+
+    }
+
+    public void CreateConstructElem()
+    {
+        SwitchPlacingMode();
+
+        constructElems[0].transform.localScale = new Vector3(GrideController.firstElem.transform.localScale.x * 5, GrideController.firstElem.transform.localScale.y * 5, GrideController.firstElem.transform.localScale.z * 5);
+        currentGridElemSelected = GrideController.firstElem;
+        currentElemSelected = Instantiate(constructElems[0], new Vector3(currentGridElemSelected.transform.position.x, constructElems[0].gameObject.transform.localScale.y / 2, currentGridElemSelected.transform.position.z), currentGridElemSelected.transform.rotation);
+        currentGridElemSelected.TryPlaceItem(false);
+    }
+}
---
+++ Assets/Scripts/GrideController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrideController : MonoBehaviour
+{
+    public int gridElemsCount = 0;
+    private int oldGridElemsCount = 0;
+
+    public float gridElemsSize = 0.1f;
+
+    private GridElemController[,] __gridElems;
+    public static GridElemController firstElem;
+    public GridElemController ge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGrid();
+        ColorizeAllGrid();
+    }
+
+    private void CreateGrid()
+    {
+        oldGridElemsCount = gridElemsCount;
+        __gridElems = new GridElemController[gridElemsCount, gridElemsCount];
+        ge.transform.localScale = new Vector3(gridElemsSize, gridElemsSize, gridElemsSize);
+        Vector3 gridStartPos = (new Vector3(-0.5f * gridElemsSize * 10 * gridElemsCount / 2, 0, 0.5f * gridElemsSize * 10 * gridElemsCount / 2));
+        Vector3 gridPos = new Vector3(gridStartPos.x, gridStartPos.y, gridStartPos.z);
+        for (int i = 0; i < gridElemsCount; i++)
+        {
+            for (int j = 0; j < gridElemsCount; j++)
+            {
+                GridElemController getedElem = Instantiate(ge, gridPos, new Quaternion(0, 0, 0, 0));
+                __gridElems[i, j] = getedElem;
+
+                gridPos = new Vector3(gridPos.x + gridElemsSize * 10, gridPos.y, gridPos.z);
+            }
+
+            gridPos = new Vector3(gridStartPos.x, gridPos.y, gridPos.z + gridElemsSize * 10);
+        }
+
+        firstElem = __gridElems[0, 0];
+    }
+
+    private void UpdateGrid()
+    {
+        ge.transform.localScale = new Vector3(gridElemsSize, gridElemsSize, gridElemsSize);
+        Vector3 gridStartPos = (new Vector3(-0.5f * gridElemsSize * 10 * gridElemsCount / 2, 0, 0.5f * gridElemsSize * 10 * gridElemsCount / 2));
+        Vector3 gridPos = new Vector3(gridStartPos.x, gridStartPos.y, gridStartPos.z);
+
+        if (oldGridElemsCount != gridElemsCount)
+        {
+            for (int i = 0; i < oldGridElemsCount; i++)
+            {
+                for (int j = 0; j < oldGridElemsCount; j++)
+                {
+                    Destroy(__gridElems[i, j].gameObject);
+                }
+            }
+
+
+            CreateGrid();
+        }
+        else
+        {
+            for (int i = 0; i < gridElemsCount; i++)
+            {
+                for (int j = 0; j < gridElemsCount; j++)
+                {
+                    __gridElems[i, j].transform.localScale = new Vector3(gridElemsSize, gridElemsSize, gridElemsSize);
+                    __gridElems[i, j].transform.position = new Vector3(gridPos.x, gridPos.y, gridPos.z);
+
+                    gridPos = new Vector3(gridPos.x + gridElemsSize * 10, gridPos.y, gridPos.z);
+
+
+                }
+
+                gridPos = new Vector3(gridStartPos.x, gridPos.y, gridPos.z + gridElemsSize * 10);
+            }
+        }
+    }
+
+    private void ColorizeAllGrid()
+    {
+        if (GameManager.isConstructMode && GameManager.isColoringGrid)
+        {
+            for(int i = 0; i < __gridElems.GetLength(0); i++)
+            {
+                for(int j = 0; j < __gridElems.GetLength(1); j++)
+                {
+                    if (__gridElems[i, j].installedObject == null)
+                    {
+                        __gridElems[i, j].ChangeColor(__gridElems[i, j].buildingOKColor);
+                    }
+                    else
+                        __gridElems[i, j].ChangeColor(__gridElems[i, j].buildingBusyColor);
+
+                }
+            }
+            GameManager.isColoringGrid = false;
+        }
+        else if (!GameManager.isConstructMode && GameManager.isColoringGrid)
+        {
+            for (int i = 0; i < __gridElems.GetLength(0); i++)
+            {
+                for (int j = 0; j < __gridElems.GetLength(1); j++)
+                {
+                    __gridElems[i, j].ChangeColor(__gridElems[i, j].baseColor);
+                }       
+            }
+            GameManager.isColoringGrid = false;
+        }
+    }
+}
